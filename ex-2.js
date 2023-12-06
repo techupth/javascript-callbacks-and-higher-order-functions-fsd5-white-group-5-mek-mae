984f05b15fd838fd2cec2bf7312666372e0b0ca0
Mae Okubo <okubo.mae@gmail.com>
2023-12-06 16:40:32 +0900

    Finished  Assignment Callback

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,23 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let passAmount = 0
+  for(let i = 0 ; i<array.length ; i++) {
+    passAmount += operation(array[i])
+  }
+  if(passAmount>=5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkScore(score){
+  let over70 = 0;
+  if(score>70) {
+    over70 += 1
+  }
+  return over70
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +27,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result  = atLeastFive(studentScoresRoom1,checkScore);
+let scoreRoom2Result  = atLeastFive(studentScoresRoom2,checkScore);
+let scoreRoom3Result  = atLeastFive(studentScoresRoom3, checkScore);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
